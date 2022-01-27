27a48e491e073dafa6db009b27b0e4847b9856de
ssolis84 <santiago.solis@intsuperior.edu.ec>
2022-01-26 20:59:36 -0500

    Esta es la primera versión del Proyecto

---
+++ src/ec/edu/intsuperior/modelo/Usuario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Usuario {
+    private String nombreUsuario;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String password) {
+        this.nombreUsuario = nombreUsuario;
+        this.password = password;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author Santiago
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        FrmIngresoUsuario frmIngresoUsuario= new FrmIngresoUsuario();
+        frmIngresoUsuario.setVisible(true);
+    }
+    
+}
